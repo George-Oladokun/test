3bd456d93ec5d15eb0d623c281462f8d09758be7
George-Oladokun <oladokungeorge8@gmail.com>
2024-05-08 09:36:27 +0100

    test

---
+++ Test 1/Question 2/script.js
@@ -0,0 +1,20 @@
+// Implement a queue using an object - isFull, isEmpty, Enqeue, deqeue, size, print 
+let players = ['Palmer', 'Modyrik', 'Sterling', 'Mbappe', 'Muhammed Salah']
+class queue {
+    constructor() {
+        this.all = []
+    }
+    enqueue(student) {
+        this.all.push(student)
+    }
+}
+dequeue(){
+    this.all.shift()
+
+    isEmpty(){
+
+    }
+}
+size(){
+    console.log();
+}
